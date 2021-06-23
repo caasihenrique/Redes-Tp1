2e6a09f9d8d0f4ca9774b862d2ab148292dd12d5
caasihenrique <caasihenrique@yahoo.com.br>
2021-06-23 15:59:06 -0300

    retirado excessos

--- commo.h
+++ commo.h
@@ -10,10 +10,6 @@ typedef struct{
     unsigned char var:8;
 }byte;
 
-typedef struct{
-    unsigned short int vetor:8;
-}short_int;
-
 unsigned char bit_Code_to_char(nibble var);
 
 nibble char_Code_to_bit(unsigned char var);
@@ -22,6 +18,3 @@ void encode16(unsigned char input, unsigned char* output);
 
 unsigned char decode16(unsigned char* input);
 
-void short_to_char(unsigned short int size, unsigned char* duo);
-
-uint16_t checksum(void *data, unsigned int bytes);
